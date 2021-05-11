d5236959d3b0ddd5b605bfb0e143714ba370a738
tungnthust <thanhtungbka1@gmail.com>
2021-05-11 21:55:52 +0700

    update

--- Database/homestay-booking.sql
+++ Database/homestay-booking.sql
@@ -4,6 +4,9 @@
 -- ------------------------------------------------------
 -- Server version	8.0.23
 
+CREATE SCHEMA `homestaybooking`;
+USE `homestaybooking`;
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
